a27d65195c06631b85daabbe28efc71dbf170e0b
phuongne <doanthanhphuong29012006@gmail.com>
2026-08-03 11:51:54 +0700

    Authorized

--- api/v1/routes/index.route.js
+++ api/v1/routes/index.route.js
@@ -1,10 +1,12 @@
 const taskRoute = require('../routes/task.route');
 const userRoute = require('../routes/user.route');
 
+const authMiddleware = require('../middlewares/auth.middleware');
+
 module.exports = (app) => {
     const version = "/api/v1"
 
-    app.use(version + '/tasks', taskRoute);
+    app.use(version + '/tasks', authMiddleware.requireAuth, taskRoute);
 
     app.use(version + '/users', userRoute);
 }
